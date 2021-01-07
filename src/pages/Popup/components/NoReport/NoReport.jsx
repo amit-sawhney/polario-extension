74a8abafd9bee4a984a33bcb29b553ee04247063
Nick King <nrking0@gmail.com>
2021-01-07 17:36:44 -0600

    Added Animation for not found

--- src/pages/Popup/components/NoReport/NoReport.jsx
+++ src/pages/Popup/components/NoReport/NoReport.jsx
@@ -1,5 +1,18 @@
 import React from 'react';
 import './NoReport.scss';
+import * as animation from '../../../../assets/animations/error.json';
+import Lottie from 'react-lottie';
+
+
+
+const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animation.default,
+    rendererSettings: {
+        preserveAspectRatio: "xMidYMid slice"
+    }
+};
 
 /**
  * Returns default JSX for an unkown website
@@ -8,6 +21,7 @@ const NoReport = () => {
 
     return (
         <div>
+            <Lottie options={defaultOptions} height={120} width={120} />
             <h4>No report found!</h4>
         </div>
     )
